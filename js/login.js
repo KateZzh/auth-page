853f0c012e4f1c41fc6716697423b62492a52c27
KateZzh <ekaterin.pro2@gmail.com>
2023-03-10 23:47:12 +0300

    added some changes to login.js

--- js/login.js
+++ js/login.js
@@ -1,24 +1,36 @@
+const inpLogin = document.querySelector(".emailOrNumber");
+const password = document.querySelector(".password");
+const imgRef = document.querySelector(".img-ref");
+
+function isValid(inpLogin_, password_) {
+  if (
+    !/^[A-Za-z0-9\.]+@\w{1,20}\.[A-ZZa-z]{1,10}$/gm.test(inpLogin_.value) &&
+    !/^(\+375|80|375)(44|29|33|25)[0-9]{7,7}$/gm.test(inpLogin_.value)
+  )
+    throw new Error("Неверный формат электронной почты или номера телефона");
+
+  if (password_.value.length < 8)
+    throw new Error("Пароль не должен быть менее 8 символов");
+}
+
 document.querySelector(".btn").addEventListener("click", () => {
   try {
-    const emailOrNumber = document.querySelector(".emailOrNumber");
-    const password = document.querySelector(".password");
-    const imgRef = document.querySelector(".img-ref");
-
-    if (
-      !/^[A-Za-z0-9\.]+@\w{1,20}\.[A-ZZa-z]{1,10}$/gm.test(emailOrNumber.value) && !/^\+375(44|29|33|25)[0-9]{1,7}$/gm.test(emailOrNumber.value)) {
-      emailOrNumber.style = "border: 1px solid red";
-      imgRef.style = "background-image: url(../assets/img-refRed.png)";
-      throw new Error("Неверный формат электронной почты или номера телефона");
-    }
-
-    if (password.value.length < 8) {
-      password.style = "border: 1px solid red";
-      imgRef.style = "background-image: url(../assets/img-refRed.png)";
-      throw new Error("Пароль не должен быть менее 8 символов");
-    }
+    isValid(inpLogin, password);
 
     alert("Вы успешно авторизованы в системе");
+
+    inpLogin.value = "";
+    password.value = "";
   } catch (error) {
+    if (
+      error.message == "Неверный формат электронной почты или номера телефона"
+    ) {
+      inpLogin.style = "border: 1px solid red";
+    } else if (error.message == "Пароль не должен быть менее 8 символов") {
+      password.style = "border: 1px solid red";
+    }
+    imgRef.style = "background-image: url(../assets/img-refRed.png)";
+
     alert(error.message);
   }
 });
